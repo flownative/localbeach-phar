8159d0d8f2114bbd7e815396a62fff2d7e207c5e
Karsten Dambekalns <karsten@dambekalns.de>
2018-09-19 10:50:06 +0200

    Add resource:upload command

--- src/Flownative/Beach/Cli/Command/Resources/UploadCommand.php
+++ src/Flownative/Beach/Cli/Command/Resources/UploadCommand.php
@@ -4,7 +4,6 @@ namespace Flownative\Beach\Cli\Command\Resources;
 use Flownative\Beach\Cli\Command\BaseCommand;
 use Flownative\Beach\Cli\Service\ConfigurationService;
 use Google\Cloud\Core\ServiceBuilder;
-use Google\Cloud\Storage\StorageObject;
 use Neos\Utility\Exception\FilesException;
 use Neos\Utility\Files;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,7 +11,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class DownloadCommand extends BaseCommand
+class UploadCommand extends BaseCommand
 {
     /**
      * @var ConfigurationService
@@ -34,13 +33,13 @@ class DownloadCommand extends BaseCommand
     protected function configure()
     {
         $this
-            ->setName('resource:download')
-            ->setDescription('Download resources (assets) from Beach to a local Flow or Neos installation')
+            ->setName('resource:upload')
+            ->setDescription('Upload resources (assets) from a local Flow or Neos installation to Beach')
             ->setHelp(
-"The <info>resource:download</info> command downloads Flow resources from a Beach instance to
-a local Flow or Neos project.
+"The <info>resource:upload</info> command uploads Flow resources from a local Flow or Neos
+project to a Beach instance.
 
-Resource data (that is, the actual files containing binary data) will be downloaded to the
+Resource data (that is, the actual files containing binary data) will be uploaded from the
 <info>Data/Persistent/Resources</info> directory.
 
 It is your responsibility to make sure that the database content is matching this data. Be
@@ -48,9 +47,9 @@ aware that Neos and Flow keep track of existing resources by a database table.
 If resources are not registered in there, Flow does not know about them.
 
 Use this command by switching to the root directory of your Flow or Neos installation and
-then running <info>resource:download</info> and specify the instance identifier.
+then running <info>resource:upload</info> and specify the instance identifier.
 
-Note: Existing data in the local project instance will be left unchanged.
+Note: Existing data in the Beach instance will be left unchanged.
 "
             )
             ->addOption('instance', 'i', InputOption::VALUE_REQUIRED, 'Instance identifier, e.g. "instance-123abcde-456-abcd-1234-abcdef012345"');
@@ -74,7 +73,9 @@ Note: Existing data in the local project instance will be left unchanged.
         }
         $localResourcesPath = $localPersistentPath . 'Resources/';
         if (!file_exists($localResourcesPath)) {
-            mkdir($localResourcesPath);
+            $io->error(sprintf('The path %s does not exist.', $localResourcesPath));
+            $io->text('Please run this command from the root directory of your Flow or Neos installation.');
+            return 1;
         }
 
         $instanceIdentifier = $input->getOption('instance');
@@ -110,39 +111,26 @@ Note: Existing data in the local project instance will be left unchanged.
         $bucket = $googleCloudStorage->bucket($bucketName);
 
         $io->text([
-            "Downloading resources from bucket",
-            "<info>$bucketName</info>",
-            "to local directory",
-            "<info>$localResourcesPath</info>"
+            "Uploading resources from local directory",
+            "<info>$localResourcesPath</info>",
+            "to bucket",
+            "<info>$bucketName</info>"
         ]);
         $io->newLine();
 
         $io->progressStart();
-        foreach ($bucket->objects() as $storageObject) {
+        foreach (Files::getRecursiveDirectoryGenerator($localResourcesPath) as $sourcePathAndFilename) {
             $io->progressAdvance();
-            /** @var StorageObject $storageObject */
-            $targetPathAndFilename = $localResourcesPath . $this->getRelativePathAndFilenameByHash($storageObject->name());
-            if (!file_exists(dirname($targetPathAndFilename))) {
-                Files::createDirectoryRecursively(dirname($targetPathAndFilename));
+            if (preg_match('/.*[a-f0-9]{40}$/', $sourcePathAndFilename) !== 1) {
+                continue;
             }
-            if (!file_exists($targetPathAndFilename)) {
-                $storageObject->downloadToFile($targetPathAndFilename);
-            }
+            $bucket->upload(fopen($sourcePathAndFilename, 'r'), [
+                'name' => basename($sourcePathAndFilename)
+            ]);
         }
         $io->progressFinish();
 
         $io->success('Done.');
         return null;
-    }
-
-    /**
-     * Determines and returns the absolute path and filename for a storage file identified by the given SHA1 hash.
-     *
-     * @param string $sha1Hash The SHA1 hash identifying the stored resource
-     * @return string The path and filename, for example "c/8/2/8/c828d0f88ce197be1aff7cc2e5e86b1244241ac6"
-     */
-    private function getRelativePathAndFilenameByHash(string $sha1Hash): string
-    {
-        return $sha1Hash[0] . '/' . $sha1Hash[1] . '/' . $sha1Hash[2] . '/' . $sha1Hash[3] . '/' . $sha1Hash;
     }
 }
