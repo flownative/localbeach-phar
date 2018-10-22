590e5d8ea4a93b9f689eed22b5e21d9a5a94e288
Robert Lemke <robert@flownative.com>
2018-10-22 10:54:25 +0200

    Setup: show error if SSH agent contains no identities

--- src/Flownative/Beach/Cli/Command/LocalBeach/SetupCommand.php
+++ src/Flownative/Beach/Cli/Command/LocalBeach/SetupCommand.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 /**
  * Start the local beach docker containers.
  */
-class SetupCommand extends BaseCommand
+final class SetupCommand extends BaseCommand
 {
     /**
      * @return void
@@ -29,11 +29,18 @@ class SetupCommand extends BaseCommand
     protected function execute(InputInterface $input, OutputInterface $output): ?int
     {
         $io = new SymfonyStyle($input, $output);
-
         $environment = getenv('BEACH_REMOTE_AUTHORIZED_KEYS') ?? null;
         if ($environment) {
             $io->success('Environment variable "BEACH_REMOTE_AUTHORIZED_KEYS" already set, doing nothing.');
             return 0;
+        }
+
+        $agentOutput = exec('ssh-add -l');
+        if (strpos($agentOutput, 'no identities') !== false) {
+            $io->error('Your SSH agent reports that it has no identities.');
+            $io->text('Please check the Flownative guide about how to create and add an SSH key to your SSH agent.');
+            $io->text('If everything is set up correctly, "ssh-add -l" should list fingerprints of your SSH key(s).');
+            return 1;
         }
 
         $shell = $this->detectShell();
@@ -62,7 +69,7 @@ class SetupCommand extends BaseCommand
     /**
      * @return string
      */
-    protected function detectShell(): string
+    private function detectShell(): string
     {
         $shell = $_SERVER['SHELL'] ?? getenv('SHELL');
         if (!empty($shell)) {
@@ -88,7 +95,7 @@ class SetupCommand extends BaseCommand
     /**
      * @return string
      */
-    protected function detectHomeDirectory(): string
+    private function detectHomeDirectory(): string
     {
         $homeDirectory = $_SERVER['HOME'] ?? getenv('HOME');
         if (empty($homeDirectory)) {
@@ -101,7 +108,7 @@ class SetupCommand extends BaseCommand
     /**
      * @param string $filename
      */
-    protected function writeEnvironment(string $filename)
+    private function writeEnvironment(string $filename)
     {
         if (!is_readable($filename)) {
             throw new RuntimeException("The file $filename was not found. Please create it manually and add the following line:\n" . $this->getEnvironmentLine(), 1536585747);
