efe31d0d0d691f6ff29338dfe46d93b7b26de0a8
Karsten Dambekalns <karsten@dambekalns.de>
2020-02-24 20:42:35 +0100

    Extract 30 lines of duplicate code into base class

--- src/Flownative/Beach/Cli/Command/Resources/UploadCommand.php
+++ src/Flownative/Beach/Cli/Command/Resources/UploadCommand.php
@@ -1,8 +1,6 @@
 <?php
 namespace Flownative\Beach\Cli\Command\Resources;
 
-use Flownative\Beach\Cli\Command\BaseCommand;
-use Google\Cloud\Core\ServiceBuilder;
 use Neos\Utility\Exception\FilesException;
 use Neos\Utility\Files;
 use Symfony\Component\Console\Input\InputInterface;
@@ -21,7 +19,7 @@ class UploadCommand extends BaseCommand
             ->setName('resource:upload')
             ->setDescription('Upload resources (assets) from a local Flow or Neos installation to Beach')
             ->setHelp(
-"The <info>resource:upload</info> command uploads Flow resources from a local Flow or Neos
+'The <info>resource:upload</info> command uploads Flow resources from a local Flow or Neos
 project to a Beach instance.
 
 Resource data (that is, the actual files containing binary data) will be uploaded from the
@@ -35,7 +33,7 @@ Use this command by switching to the root directory of your Flow or Neos install
 then running <info>resource:upload</info> and specify the instance identifier.
 
 Note: Existing data in the Beach instance will be left unchanged.
-"
+'
             )
             ->addOption('instance', 'i', InputOption::VALUE_REQUIRED, 'Instance identifier, e.g. "instance-123abcde-456-abcd-1234-abcdef012345"');
     }
@@ -63,59 +61,26 @@ Note: Existing data in the Beach instance will be left unchanged.
             return 1;
         }
 
-        $instanceIdentifier = $input->getOption('instance');
-        if (preg_match('/^instance\-[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/', $instanceIdentifier) === 0) {
-            $io->error(sprintf('The instance identifier is not valid.', $instanceIdentifier));
+        $bucket = $this->determineStorageBucketForInstance($input->getOption('instance'), $io);
+        if ($bucket === null) {
             return 1;
         }
-
-        $io->text('Retrieving cloud storage access data from instance ...');
-        $io->newLine();
-
-        $environmentVariables = [];
-        exec('ssh -J beach@ssh.flownative.cloud beach@' . $instanceIdentifier . '.beach /bin/bash -c "env | grep BEACH_GOOGLE_CLOUD_STORAGE_"', $environmentLines);
-
-        foreach ($environmentLines as $line) {
-            list($key, $value) = explode("=", $line);
-            $environmentVariables[$key] = $value;
-        }
-
-        $bucketName = '';
-        if (isset($environmentVariables['BEACH_GOOGLE_CLOUD_STORAGE_STORAGE_BUCKET'])) {
-            $bucketName = $environmentVariables['BEACH_GOOGLE_CLOUD_STORAGE_STORAGE_BUCKET'];
-        } elseif (isset($environmentVariables['BEACH_GOOGLE_CLOUD_STORAGE_PUBLIC_BUCKET'])) {
-            $bucketName = $environmentVariables['BEACH_GOOGLE_CLOUD_STORAGE_PUBLIC_BUCKET'];
-        }
-
-        if (empty($bucketName)) {
-            $io->error('Could not retrieve cloud storage information from instance.');
-            return 1;
-        }
-
-        $privateKey = json_decode(base64_decode($environmentVariables['BEACH_GOOGLE_CLOUD_STORAGE_SERVICE_ACCOUNT_PRIVATE_KEY']), true);
-
-        $googleCloud = new ServiceBuilder([
-            'keyFile' => $privateKey
-        ]);
-
-        $googleCloudStorage = $googleCloud->storage();
-        $bucket = $googleCloudStorage->bucket($bucketName);
 
         $io->text([
-            "Uploading resources from local directory",
+            'Uploading resources from local directory',
             "<info>$localResourcesPath</info>",
-            "to bucket",
-            "<info>$bucketName</info>"
+            'to bucket',
+            '<info>' . $bucket->name() . '</info>'
         ]);
         $io->newLine();
 
         $io->progressStart();
         foreach (Files::getRecursiveDirectoryGenerator($localResourcesPath) as $sourcePathAndFilename) {
             $io->progressAdvance();
-            if (preg_match('/.*[a-f0-9]{40}$/', $sourcePathAndFilename) !== 1) {
+            if (preg_match('/[a-f0-9]{40}$/', $sourcePathAndFilename) !== 1) {
                 continue;
             }
-            $bucket->upload(fopen($sourcePathAndFilename, 'r'), [
+            $bucket->upload(fopen($sourcePathAndFilename, 'rb'), [
                 'name' => basename($sourcePathAndFilename)
             ]);
         }
